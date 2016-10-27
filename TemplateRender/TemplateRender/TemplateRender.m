3654f32548d7ea0d132800e0d105e094d1d6c3ba
Boyan Yankov <boyan.yankov@stanga.net>
2016-10-27 12:28:12 +0300

    added `TemplateRender`

---
+++ TemplateRender/TemplateRender/TemplateRender.m
@@ -0,0 +1,16 @@
+//
+//  TemplateRender.m
+//  TemplateRender
+//
+//  Created by boyankov on W43 27/10/2016 Thu.
+//  Copyright © 2016 boyankov@yahoo.com. All rights reserved.
+//
+
+#import "TemplateRender.h"
+
+@implementation TemplateRender
+
++ (UIImage *)templateImageFromSourceImage:(UIImage *)sourceImage {
+    return sourceImage;
+}
+@end
